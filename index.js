34e582ea296aee7d86d212162a9ed73d128c25b7
Raxita Mathukiya <jay343421@gmail.com>
2023-08-17 18:21:13 +0530

    donee

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const app=express()
+const cors = require('cors');
 const {postRouter}=require('./Router/post.route')
 
 const {connection}=require("./db")
 require('dotenv').config()
+app.use(cors());
 app.use(express.json())
 
 
